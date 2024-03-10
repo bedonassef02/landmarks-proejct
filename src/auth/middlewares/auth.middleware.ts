b3e63d344efd79565bf4a1bbf1f8629e5fd1cb1e
Bedo <bedonassef71@gmail.com>
2024-03-10 14:55:46 +0200

    enhance auth

--- src/auth/middlewares/auth.middleware.ts
+++ src/auth/middlewares/auth.middleware.ts
@@ -8,12 +8,27 @@ import { TokenService } from '../services/token.service';
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
   constructor(private readonly tokenService: TokenService) {}
+
   async use(req: any, res: any, next: () => void): Promise<void> {
-    const { access_token } = req.cookies;
-    if (!access_token) {
-      throw new UnauthorizedException('token is required');
+    let token: string | undefined;
+
+    // Check if the token is provided in the Authorization header
+    const authHeader = req.headers['authorization'];
+    if (authHeader && authHeader.startsWith('Bearer ')) {
+      token = authHeader.split(' ')[1];
     }
-    req.user = await this.tokenService.verify(access_token);
+
+    // If the token is not in the header, check the cookies
+    if (!token) {
+      const { access_token } = req.cookies;
+      if (!access_token) {
+        throw new UnauthorizedException('Token is required');
+      }
+      token = access_token;
+    }
+
+    // Verify the token
+    req.user = await this.tokenService.verify(token);
     next();
   }
 }
